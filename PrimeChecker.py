25ec2ab6a7ea944ac0c2b18ce66f4183b04c7f0b
xianhongtao <xianhongtao2022@outlook.com>
2025-03-15 23:36:52 +0800

    添加Python程序以检查输入整数是否为质数，并更新Jupyter笔记本中的相关代码

---
+++ PrimeChecker.py
@@ -0,0 +1,33 @@
+Number = int(input("请输入一个整数："))
+IsPrime = True
+TensPlace = 1
+i = 1
+
+while (TensPlace*10)**2 < Number:
+    TensPlace += 1
+
+if Number <= 1:
+    IsPrime = False
+if Number % 2 == 0:
+    IsPrime = False
+if Number % 3 == 0:
+    IsPrime = False
+if Number % 5 == 0:
+    IsPrime = False
+if Number % 7 == 0:
+    IsPrime = False
+
+if Number in (2 , 3 , 5 , 7):
+    IsPrime = True
+
+for i in range(1 , TensPlace+1):
+    if Number % ((i*10)+1) == 0:
+        IsPrime = False
+    if Number % ((i*10)+3) == 0:
+        IsPrime = False
+    if Number % ((i*10)+7) == 0:
+        IsPrime = False
+    if Number % ((i*10)+9) == 0:
+        IsPrime = False
+
+print(IsPrime)
