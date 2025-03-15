50fa0ebac9006425f9493220c08e6e11a4871d24
xianhongtao <xianhongtao2022@outlook.com>
2025-03-15 23:58:49 +0800

    优化质数检查程序，合并条件判断以简化代码逻辑，并更新Jupyter笔记本中的相关代码

--- PrimeChecker.py
+++ PrimeChecker.py
@@ -8,26 +8,14 @@ TensPlace = math.ceil(math.sqrt(Number)/10)
 
 if Number <= 1:
     IsPrime = False
-if Number % 2 == 0:
-    IsPrime = False
-if Number % 3 == 0:
-    IsPrime = False
-if Number % 5 == 0:
-    IsPrime = False
-if Number % 7 == 0:
+if Number % 2 == 0 or Number % 3 == 0 or Number % 5 == 0 or Number % 7 == 0:
     IsPrime = False
 
 if Number in (2 , 3 , 5 , 7):
     IsPrime = True
 
 for i in range(1 , TensPlace+1):
-    if Number % ((i*10)+1) == 0:
-        IsPrime = False
-    if Number % ((i*10)+3) == 0:
-        IsPrime = False
-    if Number % ((i*10)+7) == 0:
-        IsPrime = False
-    if Number % ((i*10)+9) == 0:
+    if Number % (i*10+1) == 0 or Number % (i*10+3) == 0 or Number % (i*10+7) == 0 or Number % (i*10+9) == 0:
         IsPrime = False
 
 print(IsPrime)
