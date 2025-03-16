e40cd061a11dc33587564646323b3baa4464c50e
xianhongtao <xianhongtao2022@outlook.com>
2025-03-16 15:55:44 +0800

    优化质数检查程序，增加对质数的判断条件，并更新Jupyter笔记本中的相关代码

--- PrimeChecker.py
+++ PrimeChecker.py
@@ -11,11 +11,12 @@ if Number <= 1:
 if Number % 2 == 0 or Number % 3 == 0 or Number % 5 == 0 or Number % 7 == 0:
     IsPrime = False
 
-if Number in (2 , 3 , 5 , 7):
-    IsPrime = True
 
 for i in range(1 , TensPlace+1):
     if Number % (i*10+1) == 0 or Number % (i*10+3) == 0 or Number % (i*10+7) == 0 or Number % (i*10+9) == 0:
         IsPrime = False
+
+if Number in (2 , 3 , 5 , 7, 11 , 13 , 17 , 19):
+    IsPrime = True
 
 print(IsPrime)
