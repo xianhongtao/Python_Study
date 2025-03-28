8bd2e01593ff82361fdef37deed43b3ae0d0ba44
xianhongtao <xianhongtao2022@outlook.com>
2025-03-28 15:38:00 +0800

    添加numpy矩阵点积示例代码

---
+++ numpy_slice_example.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr1 = np.array([[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 9]])
+arr2 = np.array([[2, 4, 5],
+                 [1, 3, 6],
+                 [7, 8, 9]])
+
+result = np.dot(arr1, arr2)
+print("arr1:")
+print(arr1)
+print("arr2:")
+print(arr2)
+print("Result of dot product:")
+print(result)
