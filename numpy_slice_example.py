f763bbe90339d8ebb25afbbff8424d9e0dab2214
xianhongtao <xianhongtao2022@outlook.com>
2025-04-03 14:34:16 +0800

    优化numpy数组格式并添加Python基础程序设计笔记

--- numpy_slice_example.py
+++ numpy_slice_example.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-arr1 = np.array([[1, 2, 3],
-                 [4, 5, 6],
-                 [7, 8, 9]])
-arr2 = np.array([[2, 4, 5],
-                 [1, 3, 6],
-                 [7, 8, 9]])
+arr1 = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+arr2 = np.array([
+    [2, 4, 5],
+    [1, 3, 6],
+    [7, 8, 9]
+])
 
 result = np.dot(arr1, arr2)
 print("arr1:")
