d07b1c601341c991727ce860e648e11a5fbdbf9c
xianhongtao <xianhongtao2022@outlook.com>
2025-04-24 13:54:49 +0800

    重构PrimeChecker.py，统一变量命名风格，修正大小写不一致的问题

--- PrimeChecker.py
+++ PrimeChecker.py
@@ -1,29 +1,29 @@
 import math,time
 
-Number = int(input("请输入一个整数："))
-IsPrime = True
-MaxDivisorDigit = 1
+input = int(input("请输入一个整数："))
+is_prime = True
+max_divisor_digit = 1
 
 #开始计时
 start = time.time()
 
-MaxDivisorDigit = math.ceil(math.sqrt(Number)/10)
+max_divisor_digit = math.ceil(math.sqrt(input)/10)
 
-if Number <= 1:
-    IsPrime = False
-if Number % 2 == 0 or Number % 3 == 0 or Number % 5 == 0 or Number % 7 == 0:
-    IsPrime = False
+if input <= 1:
+    is_prime = False
+if input % 2 == 0 or input % 3 == 0 or input % 5 == 0 or input % 7 == 0:
+    is_prime = False
 
-for divisor_index in range(1 , MaxDivisorDigit+1):
-    if Number % (divisor_index*10+1) == 0 or Number % (divisor_index*10+3) == 0 or Number % (divisor_index*10+7) == 0 or Number % (divisor_index*10+9) == 0:
-        IsPrime = False
+for divisor_index in range(1 , max_divisor_digit+1):
+    if input % (divisor_index*10+1) == 0 or input % (divisor_index*10+3) == 0 or input % (divisor_index*10+7) == 0 or input % (divisor_index*10+9) == 0:
+        is_prime = False
 
-if Number in (2 , 3 , 5 , 7, 11 , 13 , 17 , 19):
-    IsPrime = True
+if input in (2 , 3 , 5 , 7, 11 , 13 , 17 , 19):
+    is_prime = True
 
 #结束计时
 end = time.time()
 
-print(IsPrime)
+print(is_prime)
 
 print("运行时间：",end-start)
